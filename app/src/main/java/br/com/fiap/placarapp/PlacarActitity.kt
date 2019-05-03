b0b4fb7e8e3ad8e2ae63261b3100fdb9b4dd7fd6
Logon Aluno <logonrmlocal@fiap.com.br>
2019-05-02 22:33:54 -0300

    alteracoes

--- app/src/main/java/br/com/fiap/placarapp/PlacarActitity.kt
+++ app/src/main/java/br/com/fiap/placarapp/PlacarActitity.kt
@@ -1,36 +1,49 @@
 package br.com.fiap.placarapp
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.AbsSavedState
-import android.view.View
 import kotlinx.android.synthetic.main.activity_placar.*
 
 class PlacarActitity : AppCompatActivity(){
 
-    private var golCasa = 0
-    private var golVisitante = 0
+
+    private lateinit var placarViewModel: PlacarViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_placar)
 
+
+
+        placarViewModel = ViewModelProviders.of(this)
+                .get(PlacarViewModel::class.java)
+
+        placarViewModel.goalHome.observe(this, Observer { goalHome ->
+            tvPlacarCasa.text = "$goalHome"
+        })
+
+        placarViewModel.goalAway.observe(this, Observer {
+            tvPlacarCasa.text = "$it"
+        })
+
         tvTimeCasa.text = intent.getStringExtra("TIME_CASA1")
         tvTimeVisitante.text = intent.getStringExtra("TIME_VISITANTE")
 
-        tvPlacarCasa.text = golCasa.toString()
-        tvPlacarVisitante.text = golVisitante.toString()
+        tvPlacarCasa.text = placarViewModel.goalHome.toString()
+        tvPlacarVisitante.text = placarViewModel.goalAway.toString()
 
-        btGolCasa.setOnClickListener{
-            golCasa++
-            tvPlacarCasa.text = golCasa.toString()
+        btGolCasa.setOnClickListener {
+            placarViewModel.goalHome()
         }
 
         btGolVisitante.setOnClickListener {
-            golVisitante++
-            tvPlacarVisitante.text = golVisitante.toString()
+            placarViewModel.goalAway()
         }
+
+
     }
 
 
